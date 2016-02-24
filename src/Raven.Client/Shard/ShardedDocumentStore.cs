6fda97f9913ad48871888d947cbd8e402cab3ad7
Ayende Rahien <ayende@ayende.com>
2016-02-24 11:26:51 +0200

    Building Raven.Client on dotnet cli

--- src/Raven.Client/Shard/ShardedDocumentStore.cs
+++ src/Raven.Client/Shard/ShardedDocumentStore.cs
@@ -4,7 +4,6 @@
 // </copyright>
 //-----------------------------------------------------------------------
 using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
